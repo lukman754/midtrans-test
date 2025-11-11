2e10ff52addc6372ac321384336d9177e464947f
Lukman Muludin <lukmanmauludin831@gmail.com>
2025-11-11 13:18:36 +0700

    test 2

--- api/create-qris.js
+++ api/create-qris.js
@@ -2,6 +2,15 @@
 // Vercel Serverless Function (Node 18+). Uses global fetch.
 
 export default async function handler(req, res) {
+  // CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed. Use POST.' });
   }
